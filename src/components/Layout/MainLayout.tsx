6452d6cbc40f8d6b0e62fb52ff35b0dd81b4326c
ashiqur-russel <ashiqurrussel2@gmail.com>
2025-02-15 02:55:26 +0100

    Fix Navbar and scrollable

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -3,9 +3,9 @@ import Navbar from "../shared/Navbar";
 import Cart from "../Cart/Cart";
 const MainLayout = () => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col ">
       <Navbar />
-      <main className="flex-1 relative">
+      <main className="flex-1 relative pt-[60px]">
         <Outlet />
 
         <div className="fixed top-0 right-0 bg-gray-100 p-4 shadow-lg">
